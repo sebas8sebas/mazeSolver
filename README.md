79fa0fb462e3b46c6e474bb117aae7c83a8e6460
Sebastian Ascoli <sa036@bucknell.edu>
2019-10-20 01:10:36 -0400

    BugFixes!

--- README.md
+++ README.md
@@ -5,19 +5,17 @@ Maze Solver
 This program takes the picture of a maze, and outputs a picture of 
 with the solution of the maze.
 
-To use the program go, run the Main.java program. It is going to ask
-you for a file name, enter the name of your maze's file 
-(which should be in the inputMazes directory), 
-the program will solve the maze and output the solution
-to the outputMazes directory.
+To use the program, run the Main.java program. It is going to ask
+you for a file path, enter the path of your maze png picture, 
+then program will solve the maze and output the solution
+your input directory.
 
 For the program to work, your image has to be a .png file,
 your maze has to be black and white, and
 your starting and endpoints must not be black, white or gray. You
 can only have 2 endpoints (start and end).
 To see some examples for proper input files, see the examples I 
-included in the inputMazes directory (which have their solutions on
-the outputMazes directory).
+included in the examples directory.
 
 If the program is not solving your maze, you can try adjusting the
 tolerance parameters for the color detecting methods in the 
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
@@ -10,14 +12,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the name of the picture with the maze you want to solve (it must be on inputMazes directory and in png format):");
-        String inputFileName = in.nextLine();
+        System.out.println("Enter the path of the picture with the maze you want to solve (it must be in png format):");
+        String inputFilePath = in.nextLine();
 
-        if (!inputFileName.substring(inputFileName.length() - 4, inputFileName.length()).equals(".png")){
-            inputFileName += ".png";
+        if (inputFilePath.length() < 5 || !inputFilePath.substring(inputFilePath.length() - 4, inputFilePath.length()).equals(".png")){
+            inputFilePath += ".png";
         }
 
-        File imageFile = new File("inputMazes/" + inputFileName);
+        File imageFile = new File(inputFilePath);
 
         BufferedImage image = ImageIO.read(imageFile);
 
@@ -27,7 +29,10 @@ public class Main {
         Maze maze = new Maze(image);
         BufferedImage sol = maze.solve();
 
-        File outputImage = new File("outputMazes/" + "output" + inputFileName);
+
+
+
+        File outputImage = new File(inputFilePath.substring(0, inputFilePath.length()-4) + "OUTPUT.png");
         ImageIO.write(sol, "png", outputImage);
 
         System.out.println("Done :)");
